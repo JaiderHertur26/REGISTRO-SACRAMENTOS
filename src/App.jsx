c16a06cbf709e1df69438e88129275b16526e8e2
Jaiderhertur26 <jaiderhertur26@gmail.com>
2026-08-19 12:09:53 -0500

    Agregados nuevos archivos y actualizaciones

--- src/App.jsx
+++ src/App.jsx
@@ -89,8 +89,6 @@ import MatrimonioPartidasPage from '@/pages/parish/MatrimonioPartidasPage';
 /* --- DECREES (CORRECTION & REPLACEMENT - CHANCERY) --- */
 import NewDecreeCorrectionPage from '@/pages/chancery/decree-correction/NewDecreeCorrectionPage';
 import NewDecreeReplacementPage from '@/pages/chancery/decree-replacement/NewDecreeReplacementPage';
-import ChanceryCorrectionDecreeListPage from '@/pages/chancery/ChanceryCorrectionDecreeListPage';
-import ChanceryReplacementDecreeListPage from '@/pages/chancery/ChanceryReplacementDecreeListPage';
 import ChanceryDecreeCorrectionViewPage from '@/pages/chancery/ChanceryDecreeCorrectionViewPage';
 import ChanceryDecreeReplacementViewPage from '@/pages/chancery/ChanceryDecreeReplacementViewPage';
 import ChanceryDecreeCorrectionEditPage from '@/pages/chancery/ChanceryDecreeCorrectionEditPage';
@@ -254,9 +252,6 @@ const AppContent = () => {
                 <Route path="/chancery/decree-replacement" element={<ProtectedRoute requiredRole="chancery"><ChanceryDecreeReplacementViewPage /></ProtectedRoute>} />
                 <Route path="/chancery/decree-replacement/view" element={<ProtectedRoute requiredRole="chancery"><ChanceryDecreeReplacementViewPage /></ProtectedRoute>} />
                 <Route path="/chancery/decree-replacement/edit" element={<ProtectedRoute requiredRole="chancery"><ChanceryDecreeReplacementEditPage /></ProtectedRoute>} />
-                
-                <Route path="/chancery/decree-correction/list" element={<ProtectedRoute requiredRole="chancery"><ChanceryCorrectionDecreeListPage /></ProtectedRoute>} />
-                <Route path="/chancery/decree-replacement/list" element={<ProtectedRoute requiredRole="chancery"><ChanceryReplacementDecreeListPage /></ProtectedRoute>} />
 
                 {/* --- ANNULMENT (CHANCERY) --- */}
                 <Route path="/chancery/decree-annulment" element={<ProtectedRoute requiredRole="chancery"><ChanceryAnnulmentConceptsPage /></ProtectedRoute>} />
